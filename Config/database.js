3827aec4dde7924d8bea3c053605df8867bfaaff
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-01-31 14:52:02 +0530

    db1

--- Config/database.js
+++ Config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const connection = mongoose.connect(process.env.MONGO_URL, {
+const connection = mongoose.connect('mongodb+srv://bansiya55:lokesh55@live-streaming.gbcm0s3.mongodb.net/QurinomSolutions?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
